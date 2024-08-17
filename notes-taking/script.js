f4aee2f200c485fbd9b1aeabc8676e2a319c31b6
Sachin <leavethesq@gmail.com>
2024-08-17 16:49:45 +0530

    #???

---
+++ notes-taking/script.js
@@ -0,0 +1,15 @@
+
+var overlaysa=document.querySelector(".overlay")
+var popupsa=document.querySelector(".popup")
+
+function add()
+{
+    overlaysa.style.display="block"
+    popupsa.style.display="block"
+}
+
+function cancel()
+{
+    overlaysa.style.display="none"
+    popupsa.style.display="none" 
+}
