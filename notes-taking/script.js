48ce6ebab3fd842fd3401cbe641b511549ae22c0
Sachin <leavethesq@gmail.com>
2024-08-17 19:53:08 +0530

    #done

--- notes-taking/script.js
+++ notes-taking/script.js
@@ -1,15 +1,41 @@
 
-var overlaysa=document.querySelector(".overlay")
-var popupsa=document.querySelector(".popup")
+var overlaysa = document.querySelector(".overlay")
+var popupsa = document.querySelector(".popup")
+var add = document.querySelector('.add')
+var cancel = document.getElementById('cancel')
 
-function add()
-{
-    overlaysa.style.display="block"
-    popupsa.style.display="block"
+add.addEventListener("click", function () {
+    overlaysa.style.display = "block"
+    popupsa.style.display = "block"
+})
+
+cancel.addEventListener("click", function () {
+    overlaysa.style.display = "none"
+    popupsa.style.display = "none"
+})
+
+function del(event) {
+    event.target.parentElement.remove()
 }
 
-function cancel()
-{
-    overlaysa.style.display="none"
-    popupsa.style.display="none" 
-}
+
+var container = document.querySelector('.container')
+var save = document.getElementById('save-rant')
+var topic = document.getElementById('topic')
+var subject = document.getElementById('subject')
+var here = document.getElementById('here')
+
+save.addEventListener("click", function (event) {
+    event.preventDefault()
+    var div= document.createElement('div')
+    div.setAttribute("class", "rant-container")
+    div.innerHTML = `<h2>${topic.value}</h2>
+    <h5>${subject.value}</h5>
+    <p>${here.value}</p>
+    <button onclick="del(event)"> DELETE</button>`
+
+    container.append(div)
+
+    overlaysa.style.display = "none"
+    popupsa.style.display = "none"
+})
